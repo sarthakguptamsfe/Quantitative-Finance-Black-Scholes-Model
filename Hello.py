5a6eee741354649ab7eb4b4be7205edb0060fc89
sarthakguptamsfe <139661843+sarthakguptamsfe@users.noreply.github.com>
2024-05-06 00:37:15 +0000

    hello#

--- Hello.py
+++ Hello.py
@@ -12,9 +12,9 @@ from py_vollib.black_scholes.greeks import analytical
 # Streamlit page setup
 st.title('Quantitative Finance: Black Scholes Model for European Option Pricing')
 
-# Display the image from Google Drive
-image_url = "https://ibb.co/Tcy1WfB"
-st.image(image_url, caption='Quantitative Finance', use_column_width=True)
+# Display the image using the direct link
+image_url = "https://i.postimg.cc/bvqxzkwp/abc.jpg"
+st.image(image_url, caption='Header Image', use_column_width=True)
 
 # API key for FinancialModelingPrep
 API_KEY = '0uTB4phKEr4dHcB2zJMmVmKUcywpkxDQ'
